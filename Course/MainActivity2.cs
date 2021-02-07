607306141a201b6975189461f402087132b8e836
Niaz Rahman Bhuiyan <niazrahmanbhuiyan@gmail.com>
2021-02-08 00:55:12 +0600

    Course Final Version

---
+++ Course/MainActivity2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    class MainActivity2
+    {
+        static void Main(string[] args)
+        {
+            Course c1 = new Course("Introduction to Programming", "110", 4);
+            Course c2 = new Course("Object Oriented Programming", "234", 4);
+
+            c1.ShowCourseInfo();
+            c2.ShowCourseInfo();
+        }
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    public class Course
+    {
+        private string courseName;
+        private string courseCode;
+        private int courseCredit;
+
+        public Course()
+        {
+
+        }
+        public Course(string courseName,string courseCode,int courseCredit)
+        {
+            this.courseName = courseName;
+            this.courseCode = courseCode;
+            this.courseCredit = courseCredit;
+        }
+        public string CourseName
+        {
+            get { return courseName; }
+            set { courseName = value; }
+        }
+        public string CourseCode
+        {
+            get { return courseCode; }
+            set { courseCode = value; }
+        }
+        public int CourseCredit
+        {
+            get { return courseCredit; }
+            set { courseCredit = value; }
+        }
+        public void ShowCourseInfo()
+        {
+            Console.WriteLine("Course Name: " + courseName);
+            Console.WriteLine("Course Code: " + courseCode);
+            Console.WriteLine("Course Credit: " + courseCredit);
+        }
+    }
+}
