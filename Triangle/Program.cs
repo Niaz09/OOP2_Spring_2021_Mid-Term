baa49f648c686cb8b08303ff7634aeaf1b82f944
Niaz Rahman Bhuiyan <niazrahmanbhuiyan@gmail.com>
2021-02-08 01:53:12 +0600

    Triangle final version

---
+++ Triangle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    public class Triangle
+    {
+        private int x;
+        private int y;
+        private int z;
+    
+    }
+    public Triangle()
+    {
+
+    }
+    public Triangle(int x, int y, int z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public void ShowInfo()
+    {
+        if (x == y)
+        {
+            Console.WriteLine("Equilateral Triangle");
+        }
+        else if (x == z)
+        {
+
+        }
+    }
+}
