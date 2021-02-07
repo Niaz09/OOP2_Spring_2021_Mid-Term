28a66f9cc4614d7ec1714c424df4f8c9d8ff0f17
Niaz Rahman Bhuiyan <niazrahmanbhuiyan@gmail.com>
2021-02-07 22:01:02 +0600

    Account final version

---
+++ Account/MainActivity1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account
+{
+    class MainActivity1
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Account("Niaz", "567", 50000);
+            Account a2 = new Account("James", "765", 20000);
+
+            a1.ShowInfo();
+            a1.Deposit(3000);
+            a1.Withdraw(23000);
+            a2.ShowInfo();
+            a2.Deposit(5000);
+            a2.Withdraw(1000);
+        }
+    }
+}
--- Account/Program.cs
+++ Account/Program.cs
@@ -6,10 +6,60 @@ using System.Threading.Tasks;
 
 namespace Account
 {
-    class Program
+    public class Account
     {
-        static void Main(string[] args)
+        private string accName;
+        private string acid;
+        private int balance;
+
+        public Account()
         {
+
         }
+        public Account(string accName,string acid,int balance)
+        {
+            this.accName = accName;
+            this.acid = acid;
+            this.balance = balance;
+        }
+        public string AccName
+        {
+            get { return accName; }
+            set { accName = value; }
+        }
+        public string Acid
+        {
+            get { return acid; }
+            set { acid = value; }
+        }
+        public int Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+        public void Deposit(int amount)
+        {
+            balance = balance + amount;
+            Console.WriteLine("You have deposited Taka " + amount);
+        }
+        public void Withdraw(int amount)
+        {
+            if (balance >= amount + 1000)
+            {
+                balance = balance - amount;
+                Console.WriteLine("You have withdrawn Taka " + amount);
+            }
+            else
+            {
+                Console.WriteLine("Not enough Balance");
+            }
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Account Name: " + accName);
+            Console.WriteLine("Account ID: " + acid);
+            Console.WriteLine("Balance: " + balance);
+        }
+
     }
 }
