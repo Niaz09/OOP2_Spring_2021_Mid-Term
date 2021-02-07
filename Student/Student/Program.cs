7c619dc360830cdf0dde53bf195b3a80f7e0be3f
Niaz Rahman Bhuiyan <niazrahmanbhuiyan@gmail.com>
2021-02-07 14:31:00 +0600

    Student Mark Distribution

---
+++ Student/Student/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public string name;
+            public string id;
+            public string department;
+            public float cgpa;
+
+        public Student()
+        {
+
+        }
+        public Student()
+
+
+
+
+        }
+}
+}
