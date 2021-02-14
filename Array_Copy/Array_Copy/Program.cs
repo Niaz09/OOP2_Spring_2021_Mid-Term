cad265c881e759db4690bae76e02bef1b80da6b4
Niaz Rahman Bhuiyan <niazrahmanbhuiyan@gmail.com>
2021-02-14 14:05:31 +0600

    Printing Duplicate elements in an array

---
+++ Array_Copy/Array_Copy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Copy
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr2 = new int[] { 1, 2, 100, 100, 106, 1, 2 };
+            int count = 0;
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                for (int j = i + 1; j < arr2.Length; j++)
+                {
+                    if (arr2[i] == arr2[j])
+                    {
+                        count++;
+                        
+                    }
+                    
+                }
+            }
+            Console.WriteLine("Total Number of duplicate elements: ", count);
+        }
+    }
+}
