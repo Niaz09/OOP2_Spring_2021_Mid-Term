4c6b6a08f1d6b70e0cbc2658e91cfa028947d62f
Niaz Rahman Bhuiyan <niazrahmanbhuiyan@gmail.com>
2021-02-15 22:54:54 +0600

    Copy one array elements to another array

--- Copy_Array/Program.cs
+++ Copy_Array/Program.cs
@@ -10,6 +10,27 @@ namespace Copy_Array
     {
         static void Main(string[] args)
         {
+            int[] arr1 = new int[] { 10, 20, 30, 40, 50 };
+            int[] arr2 = new int[5];
+
+            Console.Write("Array 1: ");
+            for(int i = 0; i < arr1.Length; i++) 
+            {
+                Console.Write("\t");
+                Console.Write(arr1[i]);
+            }
+            for(int i = 0; i < arr1.Length; i++)
+            {
+                arr2[i] = arr1[i];
+            }
+            Console.Write("\nArray 2: ");
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.Write("\t");
+                Console.Write(arr2[i]);
+            }
         }
+        
+
     }
 }
