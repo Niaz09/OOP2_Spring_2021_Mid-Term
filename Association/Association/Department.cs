51c177ab6d0cfdcb57b06e35dc11b9109ec54dae
Niaz Rahman Bhuiyan <niazrahmanbhuiyan@gmail.com>
2021-02-28 14:01:21 +0600

    Assosciation

---
+++ Association/Association/Department.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Association
+{
+    class Department
+    {
+        private string dept;
+
+        public string Dept
+        {
+            get { return dept; }
+            set { dept = value; }
+        }
+        private string id;
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public Department() { }
+        public Department(string dept, string id)
+        {
+            this.dept = dept;
+            this.id = id;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Department: " + dept);
+            Console.WriteLine("ID: " + id);
+        }
+      
+    }
+}
---
+++ Association/Association/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Association
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Department d1 = new Department("Computer Science", "cs");
+            Student s1 = new Student("Niaz", "s234", 2.45f);
+            s1.Department = d1;
+            s1.ShowInfo();
+        }
+    }
+}
